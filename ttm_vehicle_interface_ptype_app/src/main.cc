7412f0d256f3b4ef8bb2bc73e7387e01bdf089d4
ravitejaroyal <53802212+ravitejaroyal@users.noreply.github.com>
2021-03-14 18:25:16 -0700

    Update main.cc

--- ttm_vehicle_interface_ptype_app/src/main.cc
+++ ttm_vehicle_interface_ptype_app/src/main.cc
@@ -4,8 +4,13 @@
 #include <iostream>
 #include "logging/log.h"
 
+#include "mabx_data_udp.h"
+#include "ttm_data_udp.h"
+
 constexpr int32_t port_ttm_initial{54000};
+constexpr int32_t port_dat_fw{5000};
 constexpr char ip_ttm[] {"127.0.0.1"};
+constexpr char ip_dat_fw[] {"10.0.0.193"};
 constexpr int16_t default_main_sleep_ms{100};
 constexpr char ttm_vehicle_id[] {"199"};
 constexpr int16_t exit_signal{2};
@@ -32,14 +37,6 @@ int main(int argc, char** argv) {
         std::this_thread::sleep_for(std::chrono::milliseconds(default_main_sleep_ms));
     }
 
-    //led.setColor(0x00,0xff); // set LED color to green
-
-    //std::string ttm_connect_msg = json({{"veh_id", ttm_vehicle_id}}).dump();
-    /*json j_veh_id = 
-    { 
-        {"veh_id", TTM_VEH_ID }
-    };*/
-    //std::string ttmConnectMsg = j_veh_id.value("veh_id", "none");
     std::string ttm_connect_msg = ttm_vehicle_id;
     ttmStartupClient.sendData(ttm_connect_msg.c_str(), ttm_connect_msg.length());
 
@@ -49,12 +46,23 @@ int main(int argc, char** argv) {
         LOG(INFO) << "Waiting to recieve port number for UDP connection";
 
     }
-    //std::string ttm_rx_port = json::parse(std::string(port_msg, port_msg_size))["port"];
+    
     std::string ttm_rx_port = port_msg;
     LOG(DEBUG) << "TTM listen port: " << ttm_rx_port;
     
     ttmStartupClient.shutdownSocket();
     LOG(INFO) << "Shutdown ttm tcp socket";
-    return 0;
-}
 
+    MabxData udp;
+    TtmData ttm;
+
+    if (!udp.init(port_dat_fw, ip_dat_fw, port_dat_fw)) {
+         LOG(DEBUG) << "MABX init fail: " << std::endl;
+        return -1;
+    }
+
+    if (!ttm.init(std::stoi(ttm_rx_port), ip_ttm, std::stoi(ttm_rx_port)+1)) {
+        LOG(DEBUG) << "TTM init fail: " << std::endl;
+        return -1;
+    }
+}
